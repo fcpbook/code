2a96300fc90fcd5c3d2cc48cbba386e3189df0d6
Michel Charpentier <Michel.Charpentier@cs.unh.edu>
2023-02-09 09:27:26 -0500

    Clean zipper code.

--- Scala/main/chap05/Section_6.scala
+++ Scala/main/chap05/Section_6.scala
@@ -9,27 +9,22 @@ object Section_6:
       require(list.nonEmpty, "no empty zipper")
       (Nil, list)
 
-   def toList[A](zipper: Zipper[A]): List[A] =
-      zipper match
-         case (left, right) => left.reverse ::: right
+   def toList[A](zipper: Zipper[A]): List[A] = zipper match
+      case (left, right) => left.reverse ::: right
 
-   def get[A](zipper: Zipper[A]): A =
-      (zipper: @unchecked) match
-         case (_, x :: _) => x
+   def get[A](zipper: Zipper[A]): A = (zipper: @unchecked) match
+      case (_, x :: _) => x
 
-   def set[A](zipper: Zipper[A], value: A): Zipper[A] = zipper match
+   def set[A](zipper: Zipper[A], value: A): Zipper[A] = (zipper: @unchecked) match
       case (left, _ :: right) => (left, value :: right)
-      case _                  => throw new AssertionError("empty zipper")
 
-   def moveLeft[A](zipper: Zipper[A]): Zipper[A] =
-      zipper match
-         case (x :: left, right) => (left, x :: right)
-         case _                  => zipper
+   def moveLeft[A](zipper: Zipper[A]): Zipper[A] = zipper match
+      case (x :: left, right) => (left, x :: right)
+      case _                  => zipper
 
-   def moveRight[A](zipper: Zipper[A]): Zipper[A] =
-      zipper match
-         case (left, x :: right) if right.nonEmpty => (x :: left, right)
-         case _                                    => zipper
+   def moveRight[A](zipper: Zipper[A]): Zipper[A] = zipper match
+      case (left, x :: right) if right.nonEmpty => (x :: left, right)
+      case _                                    => zipper
 
    object NoGuardVariant:
       def moveRight[A](zipper: Zipper[A]): Zipper[A] = zipper match
